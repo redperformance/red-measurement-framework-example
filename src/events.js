bf82bb5e5c8298ecbb2a80a971b3001a6824c223
Asim Tariq <asim@redperformance.no>
2018-08-30 16:46:17 +0200

    Initiated example usecase for red-measurement-framework

---
+++ src/events.js
@@ -0,0 +1,18 @@
+export const CONTENT_VIEWED = 'Content Viewed'; // Content Viewed = Event Action
+export const INTERESTED_EVENT = 'Example Interested Event';
+export const INTERESTED_EVENT_TWO = 'Example Interested Event Two';
+export const CONSIDERING_EVENT = 'Example Considering Event';
+export const STORE_VISIT_INTENT_EVENT = 'Example Store Visit Intent Event';
+export const CART_VIEWED = 'Cart Viewed';
+export const ORDER_COMPLETED = 'Order Completed';
+
+// Important that the index match as above. Else it wont work or your report will not have correct values
+export default [
+    CONTENT_VIEWED,
+    INTERESTED_EVENT,
+    INTERESTED_EVENT_TWO,
+    CONSIDERING_EVENT,
+    STORE_VISIT_INTENT_EVENT,
+    CART_VIEWED,
+    ORDER_COMPLETED
+];
---
+++ src/measurement-framework.js
@@ -0,0 +1,74 @@
+import MeasurementFramework, {
+    contentEngaged,
+    userTrail,
+    clientIdSetter,
+    customTask,
+    ajaxComplete
+} from "measurement-framework"
+import * as event from "./events";
+import events from "./events";
+import {greater} from "measurement-framework/lib/userTrail";
+
+
+// Content Viewed (contentEngagement) Event Name
+let eventName = "Content Viewed"
+// Content Viewed (contentEngagement) Event Timer
+let secondsToTrigger = 20
+
+// contentEngagement Example for debug
+contentEngaged(eventName, secondsToTrigger, function (dataLayerPush) {
+    dataLayerPush.debugText = "Target time for Content Viewed is 20 seconds."
+})
+
+
+// customTask Example
+customTask(function (model) {
+    let tid = model.get("&tid");
+    console.log("GATID: ", tid);
+});
+
+
+// clientIdSetter Example. 1 = index in Google Analytics/Google Tag Manager
+clientIdSetter(1);
+
+
+//Usertrail Example
+userTrail(events, [
+    function () {
+        return "Aware"; // Aware if you just visit
+    },
+    function () {
+        return (
+            greater(event.CONTENT_VIEWED, 0) || // 0 = greater than 0
+            greater(event.INTERESTED_EVENT, 0) ||
+            greater(event.INTERESTED_EVENT_TWO, 0)
+            // 0 = greater than 0
+        ) ? "Interested" : false;
+    },
+    function () {
+        return (
+            greater(event.CONSIDERING_EVENT, 1)
+        ) ? "Considering" : false;
+    },
+    function () {
+        return (
+            greater(event.STORE_VISIT_INTENT_EVENT, 0)
+        ) ? "Store Visit Intent" : false;
+
+    },
+    function () {
+        return (
+            greater(event.CART_VIEWED, 0)
+        ) ? "Ecommerce Intent" : false;
+
+    },
+    function () {
+        return (
+            greater(event.ORDER_COMPLETED, 0)
+        ) ? "Ecommerce Purchase" : false;
+
+    }
+]);
+
+
+MeasurementFramework.init()
